b1ea32a278ca90d25983c3d85db1f889a435b71e
BrunoHenrique0ifsp <138006640+BrunoHenrique0ifsp@users.noreply.github.com>
2025-10-15 20:08:16 -0300

    Add files via upload

---
+++ academia/include/produto_principal.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/produto_principal.css">
+</head>
+<body>
+    <div class= "principal">
+        <div class= "produto"></div>
+    </div>
+</body>
+</html>
---
+++ academia/include/topo.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/topo.css">
+</head>
+<body>
+    <div class = "principal">
+        <div class ="plano"><img src="img/Plano.png" alt=""></div>
+        <div class="caixa-pesquisa">
+            <form action="sql/pesquisa.php" method="get">
+                <input type="text" placeholder="Pesquisar...">
+                <button type="submit">
+                    <img src="img/lupa.png" alt="Pesquisar">
+                </button>
+            </form>
+        </div>
+
+        <div class ="direita">
+            <div class ="login"><a href="login.html"><img src="img/Login.png" alt="Logar"></a>
+           <a href="login.html"> <p>Logar</p></a>
+        </div>
+            <div class="carrinho"><a href="carrinho.php"><img src="img/Compra.png" alt="Compra"></a>
+            <a href="carrinho.php"><p>carrinho</p></a>
+        </div>
+        </div>
+    </div>
+</body>
+</html>
